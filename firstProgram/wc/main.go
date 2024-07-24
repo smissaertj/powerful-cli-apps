e735cb8b6f743e992cb7124ada8110f697a4009a
Joeri Smissaert <smissaertj@gmail.com>
2024-07-24 18:07:32 +0400

    Added flag to count bytes

--- firstProgram/wc/main.go
+++ firstProgram/wc/main.go
@@ -12,18 +12,25 @@ func main() {
 
 	// Define a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+	count, err := count(os.Stdin, *lines, *bytes)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(count)
 }
 
-func count(reader io.Reader, countLines bool) (int, error) {
+func count(reader io.Reader, countLines bool, countBytes bool) (int, error) {
 
 	// Use a scanner to read text from an io.Reader interface
 	scanner := bufio.NewScanner(reader)
 
 	// Set the scanner split type to "words", default is split by lines.
-	if !countLines {
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
+	} else if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	// Define a counter
--- firstProgram/wc/main_test.go
+++ firstProgram/wc/main_test.go
@@ -9,7 +9,7 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("Word1 Word2 Word3 Word4 Word5 Word6 Word7 Word8 Word9\n")
 	exp := 9
-	res, _ := count(b, false)
+	res, _ := count(b, false, false)
 	if res != exp {
 		t.Errorf("Expected %d, got %d", exp, res)
 	}
@@ -18,8 +18,17 @@ func TestCountWords(t *testing.T) {
 func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("Word1 Word2\nWord3 Word4\nWord5 Word6\nWord7 Word8 Word9\n")
 	exp := 4
-	res, _ := count(b, true)
+	res, _ := count(b, true, false)
 	if res != exp {
 		t.Errorf("Expected %d, got %d", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBuffer([]byte("Hello World"))
+	exp := b.String()
+	res, _ := count(b, false, true)
+	if res != len(exp) { // len returns the count of bytes in the string representation of the buffer.
+		t.Errorf("Expected %d, got %d", len(exp), res)
 	}
 }
